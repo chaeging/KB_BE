06b368bf9af2f8be309c83aa0643a8e045ae3abc
동욱 <du123kim@naver.com>
2025-07-23 22:20:31 +0900

    Feature: 회원가입 구현 완료 (#36)

--- src/main/java/org/scoula/service/UserService.java
+++ src/main/java/org/scoula/service/UserService.java
@@ -1,9 +1,11 @@
 package org.scoula.service;
-
 import lombok.RequiredArgsConstructor;
 import org.scoula.mapper.UserMapper;
+import org.scoula.security.dto.AuthDTO;
 import org.scoula.security.dto.MemberDTO;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -11,20 +13,33 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserService {
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
     public MemberDTO getUser(String email) {
         return userMapper.findById(email);
     }
 
-    public List<MemberDTO > getAllUsers() {
+    public List<MemberDTO> getAllUsers() {
         return userMapper.findAll();
     }
 
-    public void addUser(MemberDTO  user) {
-        userMapper.insertUser(user);
+
+    @Transactional
+    public void signUp(MemberDTO memberDTO) {
+        // 1. 사용자 등록
+        String originalPassword = memberDTO.getPassword();
+        String newPassword = passwordEncoder.encode(originalPassword);
+        memberDTO.setPassword(newPassword);
+        userMapper.insertUser(memberDTO);
+
+        // 2. 권한 등록
+        AuthDTO authDTO = new AuthDTO();
+        authDTO.setUsersIdx(memberDTO.getUsersIdx());
+        authDTO.setAuth("ROLE_MEMBER");
+        userMapper.insertAuth(authDTO);
     }
 
-    public void updateUser(MemberDTO  user) {
+    public void updateUser(MemberDTO user) {
         userMapper.updateUser(user);
     }
 
@@ -32,8 +47,9 @@ public class UserService {
         userMapper.deleteUser(id);
     }
 
-    public MemberDTO  findByUsername(String username) {
+    public MemberDTO findByUsername(String username) {
         return userMapper.findByUsername(username);
     }
 }
+
 
