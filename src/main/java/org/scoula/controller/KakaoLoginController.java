c0810252ccd65d2f1a8a51390ec561c84e750e63
rlawlals119 <rlawlals170@naver.com>
2025-07-23 15:41:33 +0900

    fix:KakaoLoginController수정

--- src/main/java/org/scoula/controller/KakaoLoginController.java
+++ src/main/java/org/scoula/controller/KakaoLoginController.java
@@ -6,14 +6,12 @@ import org.scoula.dto.oauth.KakaoUserInfoDto;
 import org.scoula.service.oauth.KakaoOauthService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/api/kakao/")
 public class KakaoLoginController {
 
     private final KakaoOauthService kakaoOauthService;
@@ -23,12 +21,17 @@ public class KakaoLoginController {
      * @param code 카카오에서 전달하는 인가 코드
      * @return KakaoUserInfoDto + JWT
      */
-    @GetMapping("v1/auth/kakao/callback")
+    @GetMapping("oauth/callback")
     @ResponseBody
     public ResponseEntity<KakaoUserInfoDto> kakaoLogin(@RequestParam("code") String code) {
         log.info("카카오 인가 코드 수신: {}", code);
 
         KakaoUserInfoDto userInfo = kakaoOauthService.processKakaoLogin(code);
         return ResponseEntity.ok(userInfo);
+    }
+
+    @GetMapping("oauth/login")
+    public String redirectToKakao() {
+        return "redirect:https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=5a743890c223b81e0e2ca139881c8cb7&redirect_uri=http://localhost:8080/v1/auth/kakao/callback";
     }
 }
