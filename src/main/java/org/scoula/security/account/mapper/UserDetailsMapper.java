8ec69ef45076a89ace95942d84c375762f073c5f
동욱 <du123kim@naver.com>
2025-07-27 23:20:07 +0900

    Feature : 비밀번호 변경 기능 + Security 수정 (jwt 관련)(#81 , #90)

--- src/main/java/org/scoula/security/account/mapper/UserDetailsMapper.java
+++ src/main/java/org/scoula/security/account/mapper/UserDetailsMapper.java
@@ -2,7 +2,6 @@ package org.scoula.security.account.mapper;
 
 import org.scoula.security.dto.LoginDTO;
 import org.scoula.security.dto.MemberDTO;
-import org.scoula.security.dto.RefreshTokenDTO;
 
 public interface UserDetailsMapper {
     public MemberDTO get(String username);
