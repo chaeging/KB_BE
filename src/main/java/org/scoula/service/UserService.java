3cd5c51071f0c76d8edfb3eed8a90d70caf6cfdb
rlawlals119 <rlawlals170@naver.com>
2025-07-29 09:14:06 +0900

    로컬 변경사항 임시 커밋

--- src/main/java/org/scoula/service/UserService.java
+++ src/main/java/org/scoula/service/UserService.java
@@ -23,12 +23,16 @@ public class UserService {
         return userMapper.findAll();
     }
 
+
     @Transactional
     public void signUp(MemberDTO memberDTO) {
+
         String originalPassword = memberDTO.getPassword();
         String newPassword = passwordEncoder.encode(originalPassword);
         memberDTO.setPassword(newPassword);
         userMapper.insertUser(memberDTO);
+
+        // 2. 권한 등록
         AuthDTO authDTO = new AuthDTO();
         authDTO.setUsersIdx(memberDTO.getUsersIdx());
         authDTO.setAuth("ROLE_MEMBER");
@@ -39,6 +43,7 @@ public class UserService {
         userMapper.updateUser(user);
     }
 
+
     public void deleteUser(String userId) {
         userMapper.deleteUser(userId);
     }
@@ -46,35 +51,6 @@ public class UserService {
     public MemberDTO findByUsername(String username) {
         return userMapper.findByUsername(username);
     }
-
-    public Integer findUserIdxByUserId (String usersId) {
-        return userMapper.findUserIdxByUserId(usersId);
-    }
-
-    public void updatePassword(String userId, String oldPassword, String newPassword) {
-        // 1. 기존 사용자 조회
-        MemberDTO member = userMapper.findById(userId);
-        if (member == null) {
-            throw new IllegalStateException("비밀번호 변경 실패: 해당 사용자가 존재하지 않음");
-        }
-
-        // 2. 기존 비밀번호 비교
-        if (!passwordEncoder.matches(oldPassword, member.getPassword())) {
-            throw new IllegalArgumentException("기존 비밀번호가 일치하지 않습니다.");
-        }
-
-        // 3. 새 비밀번호 암호화 후 업데이트
-        String encodedNewPassword = passwordEncoder.encode(newPassword);
-        member.setPassword(encodedNewPassword);
-
-        int updated = userMapper.updatePasswordByUsername(member);
-        if (updated == 0) {
-            throw new IllegalStateException("비밀번호 변경 실패: 업데이트 실패");
-        }
-    }
-
-
-
 }
 
 
